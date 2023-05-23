8827b9dfb8e86f4c85d4231e3e7e245f5d16b1d7
vfzac <vfzac@gmail.com>
2023-05-23 15:10:28 +0800

    y axis(servo) done

--- laseeye.ino
+++ laseeye.ino
@@ -4,7 +4,6 @@
 Servo servo1;
 int RECV_PIN = 2;
 int SERVO_PIN = 3;
-const int iUp = 24, iLeft = 8, iRight = 90, iDown = 82; //refers to the command code designated to be arrow keys
 int iRemoteCommand[21] = 
   {69,70,71,
    68,64,67,
@@ -13,6 +12,8 @@ int iRemoteCommand[21] =
    12,24,94,
    8 ,28,90,
    66,82,74}; //DEC codes for the specific remote sorted from left to right, up to down
+const int iUp = 70, iLeft = 68, iRight = 67, iDown = 21,
+          iSkipUp = 69,iSkipDown = 71; //keybind
 
 void setup(){
   Serial.begin(9600);
@@ -43,25 +44,35 @@ void loop(){
 //IRremote input function
 int IR_Remote_F(){
       int i = IrReceiver.decodedIRData.command;
-      if (i == iUp || i == iLeft || i == iRight || i == iDown){ return i; //Serial.println(IrReceiver.decodedIRData.command);}
+      if (i == iUp || i == iLeft || i == iRight || i == iDown) return i; //Serial.println(IrReceiver.decodedIRData.command);}
       else if(IrReceiver.decodedIRData.protocol == 0) return 0; //return zero if the protocol is unknown::happened when the remote is not pointed in the receiver
       else if(IrReceiver.decodedIRData.flags == 0){ //zero here means there's no repeat for holding down the remote button
-       IrReceiver.printIRResultShort(&Serial); //print the data to serial
+       //IrReceiver.printIRResultShort(&Serial); //print the data to serial
        return i;
       }else {return 0;}
 }
 
 //servo handler function
 void Servo_Handler(int iCommandCodeOut){
-  int curPos = servo1.read();//save the current position of the servo
-    switch (iCommandCodeOut){
-      case iUp:
-        if(curPos<=179) curPos++;//adds to the position value by 1 *only* if the value is below 180
-        break;
-      case iDown:
-        if (curPos>=0) curPos--;//subtracts to the position value by 1 *only* if the value is above zero
-        break;
+  int iCurPos = servo1.read();//save the current position of the servo
+  int n = 3,m = 30;
+  switch (iCommandCodeOut){
+    case iUp:
+      iCurPos+= n;//adds to the position value by n *only* if the value is below 180
+      break;
+    case iDown:
+      iCurPos-= n;//subtracts to the position value by n *only* if the value is above zero
+      break;
+    case iSkipUp:
+      iCurPos+= m;
+      break;
+    case iSkipDown:
+      iCurPos-= m;
+      break;
     }
-  if(curPos != servo1.read()) servo1.write(curPos);// sets the servo position according to the new value
-  delay(150);// delays the next requests in ms
+  if(iCurPos != servo1.read() && (iCurPos <= 180 || iCurPos >=0)) servo1.write(iCurPos);// sets the servo position according to the new value
+  Serial.print(iCurPos);
+  Serial.print(" ");
+  Serial.println(servo1.read());
+  delay(10);// delays the next requests in ms
 }
